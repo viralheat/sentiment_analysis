6697defdbcdf27deb4799ae7329bab7498e74e42
Alain Ravet <alainravet@gmail.com>
2011-09-26 18:35:02 +0200

    refactor (extract method)

--- lib/sentiment_analysis/client.rb
+++ lib/sentiment_analysis/client.rb
@@ -12,14 +12,13 @@ module SentimentAnalysis
 
 
     def quota(options={})
-      query = {:api_key => @api_key}
+      query    = {:api_key => @api_key}
+      response = get_response_for(:quota, :options => options, :query => query)
 
       case options[:format]
-        when nil      then self.class.get('/quota.json', :query => query)['quota_remaining']
-        when :json    then self.class.get('/quota.json', :query => query)
-        when :xml     then self.class.get('/quota.xml',  :query => query).body
-        else
-          raise SentimentAnalysis::FormatError.new("Invalid format : #{options[:format]}")
+        when nil      then response['quota_remaining']
+        when :json    then response
+        when :xml     then response.body
       end
     end
 
@@ -27,26 +26,41 @@ module SentimentAnalysis
     def train(options)
       raise ArgumentError.new(":text parameter is missing") unless options[:text]
       raise ArgumentError.new(":mood parameter is missing") unless options[:mood]
-      query = {:api_key => @api_key, :text => options[:text], :mood => options[:mood] }
 
-      case options[:format]
-        when nil, :json then self.class.get('/train.json', :query => query)
-        when :xml       then self.class.get('/train.xml',  :query => query).body
-        else
-          raise SentimentAnalysis::FormatError.new("Invalid format : #{options[:format]}")
-      end
+      query    = {:api_key => @api_key, :text => options[:text], :mood => options[:mood] }
+      response = get_response_for(:train, :options => options, :query => query)
+
+      options[:format] == :xml ?
+        response.body :
+        response
     end
 
 
     def review(options)
       raise ArgumentError.new(":text parameter is missing") unless options[:text]
-      query = {:api_key => @api_key, :text => options[:text]}
 
-      case options[:format]
-        when nil, :json then self.class.get('/review.json', :query => query)
-        when :xml       then self.class.get('/review.xml',  :query => query).body
-        else
-          raise SentimentAnalysis::FormatError.new("Invalid format : #{options[:format]}")
+      query    = {:api_key => @api_key, :text => options[:text]}
+      response = get_response_for(:review, :options => options, :query => query)
+
+      options[:format] == :xml ?
+        response.body :
+        response
+    end
+
+  private
+
+    def get_response_for(action, params)
+      url   = url_for(action, params[:options])
+      query = params[:query]
+      self.class.get(url, :query => query)
+    end
+
+    def url_for(base, options)
+      format = options[:format] || :json
+      if [:json, :xml].include?(format)
+        "/#{base}.#{format}"
+      else
+        raise SentimentAnalysis::FormatError.new("Invalid format : #{format}")
       end
     end
 
