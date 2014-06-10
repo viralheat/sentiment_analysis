744209a9bb9c2c4816d6467cb31627f4acb79200
Jay Swain <github@jayswain.com>
2014-06-10 15:10:07 -0700

    update base API url   * remove XML support :)   * remove quota support :(

--- lib/sentiment_analysis/client.rb
+++ lib/sentiment_analysis/client.rb
@@ -4,25 +4,11 @@ module SentimentAnalysis
   class Client
     include ::HTTParty
 
-    base_uri 'viralheat.com/api/sentiment'
+    base_uri 'https://app.viralheat.com/social/api/sentiment'
 
     def initialize(options)
       @api_key = options[:api_key]
     end
-
-
-    def quota(options={})
-      query = {:api_key => @api_key}
-
-      case options[:format]
-        when nil      then self.class.get('/quota.json', :query => query)['quota_remaining']
-        when :json    then self.class.get('/quota.json', :query => query)
-        when :xml     then self.class.get('/quota.xml',  :query => query).body
-        else
-          raise SentimentAnalysis::FormatError.new("Invalid format : #{options[:format]}")
-      end
-    end
-
 
     def train(options)
       raise ArgumentError.new(":text parameter is missing") unless options[:text]
@@ -30,8 +16,7 @@ module SentimentAnalysis
       query = {:api_key => @api_key, :text => options[:text], :mood => options[:mood] }
 
       case options[:format]
-        when nil, :json then self.class.get('/train.json', :query => query)
-        when :xml       then self.class.get('/train.xml',  :query => query).body
+        when nil, :json then self.class.get('/train', :query => query)
         else
           raise SentimentAnalysis::FormatError.new("Invalid format : #{options[:format]}")
       end
@@ -43,12 +28,10 @@ module SentimentAnalysis
       query = {:api_key => @api_key, :text => options[:text]}
 
       case options[:format]
-        when nil, :json then self.class.get('/review.json', :query => query)
-        when :xml       then self.class.get('/review.xml',  :query => query).body
+        when nil, :json then self.class.get('/', :query => query)
         else
           raise SentimentAnalysis::FormatError.new("Invalid format : #{options[:format]}")
       end
     end
-
   end
 end
