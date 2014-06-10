16d3d0b4408f2e5fe036c11af85c03bd4e490965
Jay Swain <github@jayswain.com>
2014-06-10 15:07:14 -0700

    anonymize api_key for VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -10,6 +10,9 @@ VCR.config do |c|
   c.ignore_localhost         = true
   c.default_cassette_options = { :record => :none }
   c.allow_http_connections_when_no_cassette = true
+  c.filter_sensitive_data("<API_KEY>") do
+    API_KEY
+  end
 end
 
 
